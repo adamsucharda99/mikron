4daaab250141ecbd4dc6b63c5d6131c91984888d
Adam Sucharda <adamsucharda99@gmail.com>
2023-12-03 12:01:02 +0100

    added brand button variant

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,20 @@
-import { extendTheme } from '@chakra-ui/react';
+import { StyleFunctionProps, extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
   colors: { brand: '#012F87' },
+  components: {
+    Button: {
+      baseStyle: {
+        borderRadius: 'sm',
+      },
+      variants: {
+        brand: (props: StyleFunctionProps) => ({
+          bg: 'brand',
+          color: 'white',
+          _hover: { opacity: '84%' },
+          _active: { opacity: '76%' },
+        }),
+      },
+    },
+  },
 });
