6c6e47ac1b6100dee313e485543b3903da3b7b2c
Adam Sucharda <adamsucharda99@gmail.com>
2023-12-04 13:50:15 +0100

    implemented contentful

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const withNextIntl = require('next-intl/plugin')();
 
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['images.ctfassets.net'],
+  },
+};
 
 module.exports = withNextIntl(nextConfig);
---
+++ src/app/[locale]/products/[category]/[slug]/page.tsx
@@ -0,0 +1,105 @@
+import { client } from '@/contentful';
+import {
+  Button,
+  Container,
+  Flex,
+  Heading,
+  SimpleGrid,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { Entry, EntryFieldTypes } from 'contentful';
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+
+interface SeriesSkeleton {
+  contentTypeId: 'series';
+  fields: {
+    name: EntryFieldTypes.Text;
+    slug: EntryFieldTypes.Text;
+    images: EntryFieldTypes.Array<EntryFieldTypes.AssetLink>;
+    description: EntryFieldTypes.RichText;
+    catalog?: EntryFieldTypes.AssetLink;
+    category: Entry<{
+      contentTypeId: 'category';
+      fields: { name: EntryFieldTypes.Text };
+    }>;
+    manufacturer: Entry<{
+      contentTypeId: 'manufacturer';
+      fields: { name: EntryFieldTypes.Text };
+    }>;
+    seriesParameters: EntryFieldTypes.Object<Record<string, any>>;
+  };
+}
+
+interface Props {
+  params: any;
+}
+
+export async function generateStaticParams() {
+  const res = await client.getEntries({
+    content_type: 'series',
+  });
+
+  return res.items.map((item) => ({ slug: item.fields.slug }));
+}
+
+async function getSeries(slug: string, locale: string) {
+  const res = await client.getEntries<SeriesSkeleton>({
+    content_type: 'series',
+    'fields.slug': slug,
+    include: 1,
+    locale,
+  });
+
+  if (res.items.length) {
+    return res.items[0];
+  } else {
+    notFound();
+  }
+}
+
+export default async function Product({ params }: Props) {
+  const { locale, slug } = params;
+
+  const series = await getSeries(slug, locale);
+  const { name, manufacturer, images, seriesParameters, description } =
+    series.fields;
+
+  return (
+    <main>
+      <Container as='section' maxW='container.xl'>
+        <SimpleGrid columns={{ lg: 2 }} spacing={8} alignItems='center'>
+          <Image
+            src={'https:' + images[0].fields.file.url}
+            width={images[0].fields.file.details.image.width}
+            height={images[0].fields.file.details.image.height}
+            alt={name}
+          />
+          <Flex direction='column' gap={6} py={12}>
+            <Flex direction='column'>
+              <Text fontWeight='medium' color='gray.600'>
+                {manufacturer.fields.name}
+              </Text>
+              <Heading color='gray.800'>
+                {name} {locale === 'en' ? 'Series' : 'Séria'}
+              </Heading>
+            </Flex>
+            <Stack spacing={3} sx={{ li: { ml: 4 } }}>
+              {documentToReactComponents(description)}
+            </Stack>
+            <Flex gap={3} mt={2}>
+              <Button variant='brand'>
+                {locale === 'en' ? 'Contact' : 'Kontakt'}
+              </Button>
+              <Button variant='outline'>
+                {locale === 'en' ? 'Catalog' : 'Katalóg'}
+              </Button>
+            </Flex>
+          </Flex>
+        </SimpleGrid>
+      </Container>
+    </main>
+  );
+}
