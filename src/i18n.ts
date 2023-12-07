d8e1f7100b4db2a3d2f7ed6a81a8dae8cb8f2a30
Adam Sucharda <adamsucharda99@gmail.com>
2023-12-07 18:07:55 +0100

    removed messages

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,2 @@
-import { getRequestConfig } from 'next-intl/server';
-
 export const locales = ['sk', 'en'] as const;
 export const defaultLocale = 'sk';
-
-export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`./messages/${locale}.json`)).default,
-}));
