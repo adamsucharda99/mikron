6e88cc8e4f6377ba4abc37bd1b45fa11ddecad77
Adam Sucharda <adamsucharda99@gmail.com>
2023-12-13 18:39:18 +0100

    wip migrating to sanity cms

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -3,14 +3,11 @@ import { category } from './schema/documents/category';
 import { machine } from './schema/documents/machine';
 import { manufacturer } from './schema/documents/manufacturer';
 import { series } from './schema/documents/series';
-import { contact } from './schema/pages/contact';
-import { contactDepartment } from './schema/types/contactDepartment';
-import { contactPerson } from './schema/types/contactPerson';
 import { localeBlock } from './schema/types/localization/localeBlock';
 import { localeString } from './schema/types/localization/localeString';
-import { machineParameter } from './schema/types/machineParameter';
-import { seriesParameter } from './schema/types/seriesParameter';
-import { seriesParameterGroup } from './schema/types/seriesParameterGroup';
+import { machineParameter } from './schema/types/parameters/machineParameter';
+import { seriesParameter } from './schema/types/parameters/seriesParameter';
+import { seriesParameterGroup } from './schema/types/parameters/seriesParameterGroup';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
@@ -23,8 +20,5 @@ export const schema: { types: SchemaTypeDefinition[] } = {
     seriesParameterGroup,
     seriesParameter,
     machineParameter,
-    contact,
-    contactDepartment,
-    contactPerson,
   ],
 };
