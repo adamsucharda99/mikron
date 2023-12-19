9fe2b040726ea9acce555dac1596f1a520e481df
Adam Sucharda <adamsucharda99@gmail.com>
2023-12-19 21:28:08 +0100

    changed parameter table scroll button state colors

--- src/components/ParameterTable/ScrollButton.tsx
+++ src/components/ParameterTable/ScrollButton.tsx
@@ -26,10 +26,10 @@ export default function ScrollButton({ direction, tableContainerRef }: Props) {
     <IconButton
       aria-label='scroll-right'
       fontSize='40px'
-      color='rgba(30, 58, 138, 0.55)'
+      color='rgba(30, 58, 138, 0.6)'
       bg='rgba(219, 234, 254, 0.45)'
-      _hover={{ bg: 'rgba(219, 234, 254, 0.55)' }}
-      _active={{ bg: 'rgba(219, 234, 254, 0.6)' }}
+      _hover={{ bg: 'rgba(191,219,254, 0.45)' }}
+      _active={{ bg: 'rgba(147,197,253, 0.45)' }}
       borderRadius='full'
       h={12}
       w={12}
